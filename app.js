d39461aae53d5d69c773583e900c44fab2551f1e
RONNY2109 <ronnycaraballo2109@gmail.com>
2025-09-23 16:01:51 -0400

    Ruta corregidas

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.use(cookieParser());
 //RUTAS FRONTEND
 // Rutas corregidas según la estructura real
 app.use('/', express.static(path.resolve('views', 'home')));
-app.use('/styles', express.static(path.resolve('views', 'home', 'styles')));
+app.use('/styles', express.static(path.resolve('views', 'styles')));
 app.use('/signup', express.static(path.resolve('views', 'home', 'signup')));
 app.use('/login', express.static(path.resolve('views', 'home', 'login')));
 app.use('/components', express.static(path.resolve('views', 'home', 'components')));
 app.use('/img', express.static(path.resolve('img')));
-app.use('/verify/:id/token', express.static(path.resolve('views', 'home', 'components')));
+app.use('/verify/:id/token', express.static(path.resolve('views', 'home', 'components'))); // <-- corregido
 app.use(morgan('tiny'));
 
 //RUTAS BACKEND
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/**/*.html",
+    "./views/**/*.{html,js}",
     "./index.js",
     "./app.js"
   ],
